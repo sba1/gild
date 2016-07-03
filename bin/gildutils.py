617ba523fab5d671530ce3b7d4c8db1e9d0b0864
Sebastian Bauer <mail@sebastianbauer.info>
2016-07-03 10:56:04 +0200

    Some more utils added.

--- bin/gildutils.py
+++ bin/gildutils.py
@@ -4,6 +4,21 @@ import glob
 import os.path
 import sys
 
+def is_gild_root(path):
+	"""Checks if the given path is a valid gild root"""
+	urls = glob.glob(os.path.join(path,"*","*.url"))
+	return len(urls) > 0
+
+def get_gild_root():
+	"""Returns the gild root or None if none could be found."""
+	root = os.getcwd()
+	while True:
+		if is_gild_root(root): return root;
+		last_root = root
+		root = os.path.dirname(root)
+		if root == last_root: sys.exit("No gild folder structure found.")
+	return None
+
 def find_component_base(component):
 	"""Returns the base directory of the given component or exits the program"""
 	root = os.getcwd()
